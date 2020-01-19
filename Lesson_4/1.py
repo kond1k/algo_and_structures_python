b2b5d5fb0afa986e312f735ed02d449e007290d4
kondik <kond1k12@gmail.com>
2020-01-19 18:30:20 +0300

    HW 4

--- Lesson_4/1.py
+++ Lesson_4/1.py
@@ -1,5 +1,35 @@
+from timeit import Timer
+
 """
 1. Проанализировать скорость и сложность одного любого алгоритма,
 разработанных в рамках домашнего задания первых трех уроков.
 Примечание: попробуйте написать несколько реализаций алгоритма и сравнить их.
 """
+
+""" 
+3.	Сформировать из введенного числа обратное по порядку входящих в него
+цифр и вывести на экран. Например, если введено число 3486,
+ то надо вывести число 6843.
+
+"""
+
+
+def from_end_to_start_rek(num):
+    if num / 10 < 1:
+        return num
+    return num % 10 * (10 ** (len(str(num)) - 1)) + from_end_to_start_rek(num // 10)
+
+
+def from_end_to_start_cycle(num):
+    z = ''
+    while num:
+        z += str(num % 10)
+        num //= 10
+    return z
+
+
+t1 = Timer("from_end_to_start_rek(1234567)", "from __main__ import from_end_to_start_rek")
+print("rek ", t1.timeit(number=1000000), "milliseconds")
+
+t2 = Timer("from_end_to_start_cycle(1234567)", "from __main__ import from_end_to_start_cycle")
+print("cycle ", t2.timeit(number=1000000), "milliseconds")
--- Lesson_4/2.py
+++ Lesson_4/2.py
@@ -1,5 +1,54 @@
+from timeit import Timer
+
 """
 2. Написать два алгоритма нахождения i-го по счёту простого числа.
 Без использования «Решета Эратосфена»;
 Используя алгоритм «Решето Эратосфена»
 """
+
+
+def without_er(n):
+    num_f = 2
+    count = 1
+    while True:
+        check = 2
+        simple = True
+        num_f += 1
+        while check < num_f:
+            if num_f % check == 0:
+                simple = False
+            check += 1
+        if simple:
+            count += 1
+            if count == n:
+                return num_f
+
+
+def with_er(n):
+    a = [0] * 1000
+    for i in range(1000):
+        a[i] = i
+    a[1] = 0
+    m = 2
+    while m < 1000:
+        if a[m] != 0:
+            j = m * 2
+            while j < 1000:
+                a[j] = 0
+                j = j + m
+        m += 1
+
+    b = []
+    for i in a:
+        if a[i] != 0:
+            b.append(a[i])
+
+    del a
+    return b[n - 1]
+
+
+t1 = Timer("without_er(99)", "from __main__ import without_er")
+print("without ", t1.timeit(number=300), "milliseconds")
+
+t2 = Timer("with_er(99)", "from __main__ import with_er")
+print("with ", t1.timeit(number=300), "milliseconds")
