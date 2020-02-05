dc7108ee73d3d95a52f35c53fb8c6d95f8d5aaac
kondik <kond1k12@gmail.com>
2020-02-05 11:17:41 +0300

    HW8

--- Lesson_8/1.py
+++ Lesson_8/1.py
@@ -1,3 +1,58 @@
 """
 1. Закодируйте любую строку из трех слов по алгоритму Хаффмана.
 """
+
+
+class Node:
+    def __init__(self, left=None, right=None):
+        self.left = left
+        self.right = right
+
+    def children(self):
+        return self.left, self.right
+
+
+def make_haffman_tree(node, code=""):
+    if type(node) is str:
+        return {
+            node: code
+        }
+
+    l, r = node.children()
+
+    result = {}
+    # 0 - налево, 1 - направо
+    result.update(make_haffman_tree(l, code + "0"))
+    result.update(make_haffman_tree(r, code + "1"))
+
+    return result
+
+
+words = "Hello new World!"
+print("Исходная строка: " + words)
+
+frequencies = {}
+for char in words:
+    if char not in frequencies:
+        frequencies[char] = 0
+
+    frequencies[char] += 1
+
+tree = frequencies.items()
+
+while len(tree) > 1:
+    tree = sorted(tree, reverse=True, key=lambda x: x[1])
+    char1, count1 = tree[-1]
+    char2, count2 = tree[-2]
+    tree = tree[:-2]
+    tree.append(
+        (Node(char1, char2), count1 + count2)
+    )
+
+code_table = make_haffman_tree(tree[0][0])
+
+coded = []
+for char in words:
+    coded.append(code_table[char])
+
+print(coded)
--- Lesson_8/2.py
+++ Lesson_8/2.py
@@ -3,3 +3,22 @@
 Пусть дана строка S длиной N, состоящая только из маленьких латинских букв.
 Требуется найти количество различных подстрок в этой строке.
 """
+import hashlib
+
+string = input("Введите строку из малньких латинских букв\n")
+
+s = set()
+count = len(string)
+
+for i in range(len(string)):
+    if i == 0:
+        count = len(string) - 1
+    else:
+        count = len(string)
+    for j in range(count, i, -1):
+        print(string[i:j])
+        s.add(hashlib.sha1(string[i:j].encode('utf-8')).hexdigest())
+print(s)
+
+print(f"Разных подстрок в стровке {string} равно {len(s)}")
+
